6a21947064d94ed5fa8a30dcf93407b8f792e85d
AliensInvasion <n604be80x4nd5qxi2l1@gmail.com>
2021-12-08 16:01:12 +0300

    int value changed to float value in sum initialization

--- Block13/task-2/main.cpp
+++ Block13/task-2/main.cpp
@@ -8,7 +8,7 @@ int main() {
     vector<float> prices = {2.5, 4.25, 3.0, 10.0};
     vector<int> toBuy = {1, 1, 0, 3};
 
-    float sum = 0;
+    float sum = 0.f;
 
     for (int i = 0; i < toBuy.size(); ++i) {
 
