adf7a8ce4525a80465aacf608ad5651e2e5dc101
AliensInvasion <n604be80x4nd5qxi2l1@gmail.com>
2022-02-07 06:18:12 +0300

    no if solution

--- Block14/3-snake-path/main.cpp
+++ Block14/3-snake-path/main.cpp
@@ -9,16 +9,16 @@ int main() {
 
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
-
-            int temp;
-            if (i % 2 == 0) temp = j;
-            else temp = (4 - j);
-
-            number[i][temp] = count;
+            number[i][j] = count;
             ++count;
-
+        }
+        ++i;
+        for (int j = 4; i < 5 && j >= 0; --j) {
+            number[i][j] = count;
+            ++count;
         }
     }
+
 
     for (auto & i : number) {
         for (int j : i) {
