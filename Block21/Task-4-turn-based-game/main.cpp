700113caedb4f035a213de2a0de337f145b69906
AliensInvasion <n604be80x4nd5qxi2l1@gmail.com>
2022-04-15 18:16:44 +0300

    Block21 initial

--- Block21/Task-4-turn-based-game/main.cpp
+++ Block21/Task-4-turn-based-game/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <ctime>
 
 struct Character
 {
-    bool alive = true;
-    int position = 0;
     std::string name = "Unnamed";
+    int position = 0;
     int hitPoints = 1;
     int armorPoints = 0;
     int damage = 1;
+    bool alive = true;
     bool is_enemy = false;
     char skin = 0;
 };
-
 
 bool intCheck(std::string &str){
     for (int i = 0; i < str.length(); ++i) {
@@ -60,6 +60,8 @@ int intDirection (std::string &direction) {
     if (direction == "right") return 2;
     if (direction == "top") return 3;
     if (direction == "bottom") return 4;
+    if (direction == "save") return 5;
+    if (direction == "load") return 6;
     return 0;
 }
 
@@ -90,18 +92,69 @@ int move(Character &character, std::string &field, int &direction) {
 
 }
 
+bool load(Character *characters, std::string &field) {
+    std::ifstream save("..\\resources\\save.bin", std::ios::binary);
+    if (!save.is_open()) return false;
+    for (int i = 0; i < 6; ++i) {
+        int nameLength;
+        save.read((char*)&nameLength, sizeof(nameLength));
+        characters[i].name.resize(nameLength);
+        save.read((char*)characters[i].name.c_str(), nameLength);
+        save.read((char*)&characters[i].position, sizeof(characters[i].position));
+        save.read((char*)&characters[i].hitPoints, sizeof(characters[i].hitPoints));
+        save.read((char*)&characters[i].armorPoints, sizeof(characters[i].armorPoints));
+        save.read((char*)&characters[i].damage, sizeof(characters[i].damage));
+        save.read((char*)&characters[i].alive, sizeof(characters[i].alive));
+        save.read((char*)&characters[i].is_enemy, sizeof(characters[i].is_enemy));
+        save.read((char*)&characters[i].skin, sizeof(characters[i].skin));
+    }
+    int fieldLength;
+    save.read((char*)&fieldLength, sizeof(fieldLength));
+    field.resize(fieldLength);
+    save.read((char*)field.c_str(), fieldLength);
+    save.close();
+    show(field);
+    return true;
+}
+
+void save(Character *characters, std::string &field) {
+    std::ofstream save("..\\resources\\save.bin", std::ios::binary);
+    for (int i = 0; i < 6; ++i) {
+        int nameLength = characters[i].name.length();
+        save.write((char*)&nameLength, sizeof(nameLength));
+        save.write(characters[i].name.c_str(), nameLength);
+        save.write((char*)&characters[i].position, sizeof(characters[i].position));
+        save.write((char*)&characters[i].hitPoints, sizeof(characters[i].hitPoints));
+        save.write((char*)&characters[i].armorPoints, sizeof(characters[i].armorPoints));
+        save.write((char*)&characters[i].damage, sizeof(characters[i].damage));
+        save.write((char*)&characters[i].alive, sizeof(characters[i].alive));
+        save.write((char*)&characters[i].is_enemy, sizeof(characters[i].is_enemy));
+        save.write((char*)&characters[i].skin, sizeof(characters[i].skin));
+    }
+
+    int fieldLength = field.length();
+    save.write((char*)&fieldLength, sizeof(fieldLength));
+    save.write(field.c_str(), fieldLength);
+    save.close();
+}
+
 int game(Character *characters, std::string &field) {
 
     if (!characters[0].alive) return 2;
-    show(field);
     std::string str;
     std::cout << "------\n" << characters[0].name << " turn!\n------ \nYour HP = " << characters[0].hitPoints;
     std::cout << ", Your AP = " << characters[0].armorPoints << std::endl;
-    std::cout << "Choose a direction(left, right, top, bottom):" << std::endl;
+    std::cout << "Choose a direction(left, right, top, bottom) or save/load:" << std::endl;
     std::cin >> str;
     int direction = intDirection(str);
-    while (direction == 0) {
-        std::cout << "Choose a direction(left, right, top, bottom):" << std::endl;
+    while (direction > 4 || direction == 0) {
+        if (direction == 5) save(&characters[0], field);
+        if (direction == 6) {
+            load(&characters[0], field);
+            std::cout << "------\n" << characters[0].name << " turn!\n------ \nYour HP = " << characters[0].hitPoints;
+            std::cout << ", Your AP = " << characters[0].armorPoints << std::endl;
+        }
+        std::cout << "Choose a direction(left, right, top, bottom) or save/load:" << std::endl;
         std::cin >> str;
         direction = intDirection(str);
     }
@@ -151,7 +204,7 @@ int game(Character *characters, std::string &field) {
     }
     if (enemiesDead) return 1;
 
-    std::cout << "------\nEnemy turn!\n------" << std::endl;
+    std::cout << "\n------\nEnemy turn!\n------" << std::endl;
     for (int i = 1; i < 6; ++i) {
         direction = std::rand() % 4 + 1;
         position = move(characters[i], field, direction);
@@ -169,63 +222,76 @@ int game(Character *characters, std::string &field) {
             }
         }
     }
+    show(field);
     return 0;
 }
 
 int main() {
+
     std::srand(std::time(nullptr));
     std::string field;
     fillEmpty(field);
-
     Character characters[6];
-    for (int i = 0; i < 6; ++i) {
-        if (i == 0) {
-            std::string temp;
 
-            std::cout << "Enter player name:" << std::endl;
-            std::cin >> characters[i].name;
+    std::string answer;
+    std::cout << "load?(y/n)" << std::endl;
+    std::cin >> answer;
 
-            std::cout << "Enter hit points number:" << std::endl;
-            std::cin >> temp;
-            while(!intCheck(temp)) {
+    if (answer != "y") {
+
+        for (int i = 0; i < 6; ++i) {
+            if (i == 0) {
+                std::string temp;
+
+                std::cout << "Enter player name:" << std::endl;
+                std::cin >> characters[i].name;
+
                 std::cout << "Enter hit points number:" << std::endl;
                 std::cin >> temp;
-            };
-            characters[i].hitPoints = std::stoi(temp);
-            temp.clear();
+                while (!intCheck(temp)) {
+                    std::cout << "Enter hit points number:" << std::endl;
+                    std::cin >> temp;
+                };
+                characters[i].hitPoints = std::stoi(temp);
+                temp.clear();
 
-            std::cout << "Enter armor points number:" << std::endl;
-            std::cin >> temp;
-            while(!intCheck(temp)) {
                 std::cout << "Enter armor points number:" << std::endl;
                 std::cin >> temp;
-            };
-            characters[i].armorPoints = std::stoi(temp);
-            temp.clear();
+                while (!intCheck(temp)) {
+                    std::cout << "Enter armor points number:" << std::endl;
+                    std::cin >> temp;
+                };
+                characters[i].armorPoints = std::stoi(temp);
+                temp.clear();
 
-            std::cout << "Enter damage points number:" << std::endl;
-            std::cin >> temp;
-            while(!intCheck(temp)) {
                 std::cout << "Enter damage points number:" << std::endl;
                 std::cin >> temp;
-            };
-            characters[i].damage = std::stoi(temp);
-            characters[i].skin = 'P';
-        } else {
-            characters[i].name = "Enemy #" + std::to_string(i);
-            characters[i].hitPoints = (std::rand() % 100) + 51;
-            characters[i].armorPoints = std::rand() % 51;
-            characters[i].damage = (std::rand() % 15) + 16;
-            characters[i].is_enemy = true;
-            characters[i].skin = 'E';
+                while (!intCheck(temp)) {
+                    std::cout << "Enter damage points number:" << std::endl;
+                    std::cin >> temp;
+                };
+                characters[i].damage = std::stoi(temp);
+                characters[i].skin = 'P';
+            } else {
+                characters[i].name = "Enemy #" + std::to_string(i);
+                characters[i].hitPoints = (std::rand() % 100) + 51;
+                characters[i].armorPoints = std::rand() % 51;
+                characters[i].damage = (std::rand() % 15) + 16;
+                characters[i].is_enemy = true;
+                characters[i].skin = 'E';
+            }
+
         }
+        place(field, &characters[0]);
+        show(field);
 
-    }
+    } else if (!load(&characters[0], field)) return 1;
 
-    place(field, &characters[0]);
 
     int result = 0;
-    while (result == 0) result = game(&characters[0], field);
+    while (result == 0) {
+        result = game(&characters[0], field);
+    }
     if (result == 1) std::cout << "\n######\nGame over! You won!\n######" << std::endl;
     if (result == 2) std::cout << "\n######\nGame over! You are dead!\n######" << std::endl;
 
